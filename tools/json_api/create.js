22047924f343a718838ad5945a98f3e979c78432
Ehacko <Darkdav.T@gmail.com>
2021-10-23 14:19:33 -0400

    add id on create

--- tools/json_api/create.js
+++ tools/json_api/create.js
@@ -1,6 +1,7 @@
 const read = require('./read');
 function create (file_path, new_data) {
   const data = read.bind(this)(file_path);
+  new_data._id = (new Date()).valueOf()
   console.log('data =>',data)
   data.push(new_data)
   this.writeFileSync(file_path, JSON.stringify(data))
