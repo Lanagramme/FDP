856b04967151bbc8fb3478fbc38b4cd87c75718d
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2021-10-22 23:30:04 -0400

    plus d'interfaces
    interface de connexion et d'inscription, interface mj, ajout de bootstrap

---
+++ script/fdp.js
@@ -0,0 +1,204 @@
+
+class Personnage {
+	constructor(nom, classe, race, job, statistiques){
+		[ this.nom, this.classe, this.race, this.job ]=[ nom, classe, race, job ]
+		this.portrait = ''
+		this.niveau = 1
+		this.inventaire = []
+		this.or = 0
+		
+		this.statistiques = statistiques
+	}
+	structure = {
+		nom: 'string',
+		niveau: 'number',
+		portrait: 'string',
+		statistiques: 'object',
+		inventaire: 'array',
+		classe: 'string',
+		race: 'string',
+		job: 'string'
+	}
+	empoche(x){
+		this.inventaire.push(x)
+		this.inventaire.sort((a, b) => (a.nom > b.nom) ? 1 : -1)
+	}
+}
+
+class Item {
+	constructor (nom, illustration, description, prix, usage){
+		[this.nom,this.illustration,this.description ,this.prix, this.usate] = [nom, illustration, description, prix, usage]
+	}
+}
+
+function ficheDePerso(personnage){
+	const 
+		nom = {
+			element: "h1", 
+			className: 'nom',
+			innerText: personnage.nom
+		},
+		niveau = {
+			element: "div", 
+			className: 'niveau',
+			enfants: [
+				{ element: 'span', innerText: 'Niveau' },
+				{ element: 'h1', innerText: personnage.niveau }
+			]
+		},
+		tableauDesStats =()=> {
+			obj = { element : 'div', className: 'tableauDesStats', enfants: [] }
+			obj.enfants.push(
+				{
+					element: 'div', className:'life', enfants:[{element: 'div', className: 'bar'}]
+				}
+			)
+			cl(personnage)
+			cl(Object.entries(personnage.statistiques))
+			for (const [statistique, valeur ] of Object.entries(personnage.statistiques)){
+				obj.enfants.push({
+					element: 'div', 
+					className: 'stat field',
+					enfants: [
+						{ element: 'div', className: 'name', innerText: statistique}	,	
+						{ element: 'div', className: 'num', innerText: valeur },
+					]
+				})
+			}
+			return obj
+		},
+		tableauDInventaire =()=> {
+			let obj ={ element: 'div', enfants: []	}
+			
+			for (i of personnage.inventaire){
+				obj.enfants.push({
+					element: 'div', 
+					className: 'item field',
+					enfants: [
+						{ element: 'img', className: 'logo' }, 
+						{ element: 'div', className: 'name', enfants: [
+								{ element: 'p', innerText: i.nom }
+							] 
+						}
+					]
+				})
+			}
+			return obj
+		},
+		header = { element: "header", enfants: [ nom, niveau ] },
+		bandeauPresentation = {
+			element: 'div', 							
+			className: 'bandeauStats',
+			enfants: [
+				{
+					element: 'div', 
+					className: 'portrait',
+					enfant: [
+						{
+							element: 'img', 
+							className: 'image',
+							href: personnage.portrait
+						}
+					]
+				},
+				tableauDesStats()
+			]
+		},
+		bandeauIdentite = {
+			element: 'div', 
+			className: 'bandeauIdentite',
+			enfants: [
+				{ element: 'div', innerText: personnage.classe},
+				{ element: 'div', className: 'bl', innerText: personnage.race},
+				{ element: 'div', className: 'bl', innerText: personnage.job},
+			]
+		},
+		inventaire = {
+			element: 'div', 
+			className: 'inventaire',
+			enfants: [
+				{ element: 'h3', innerText: 'Inventaire' },
+				tableauDInventaire()
+			]
+		}
+
+	return {
+		element: 'div',
+		className: 'FicheDePerso',
+		enfants: [
+			header,
+			bandeauPresentation,
+			bandeauIdentite,
+			inventaire
+		]
+	}
+}
+
+function ficheItem(item){
+	const header = {
+		element: "header",
+		enfants: [
+			{
+				element: 'h3',
+				innerText: item.nom
+			}
+		]
+	},
+	illustration = {
+		element: 'img',
+		className : 'illustration',
+		href: item.illustration
+	},
+	desctiption = {
+		element: 'p',
+		innerText: item.description
+	},
+	prix = 	{
+		element: 'div',
+		className: 'prix',
+		enfants: [
+			{
+				element: 'h3',
+				innerText: item.prix
+			}
+		]
+	}
+
+	return {
+		element: 'div',
+		className: 'FicheItem',
+		enfants: [
+			header,
+			illustration,
+			desctiption,
+			prix 
+		]
+	}
+}
+
+let stat_arthur =  {
+	Force : 6,
+	Agilité : 6,
+	Intelligence : 6,
+	Chance : 7,
+	Charisme : 6
+}
+let Arthur = new Personnage('Arthur', 'Chevalier', 'Humain', 'Héro', stat_arthur)
+let Alice = new Personnage('Alice', 'Chevalier', 'Humaine', 'Epeiste', stat_arthur)
+let Alastar = new Personnage('Alastar', 'Paysan mage', 'Humaine', 'Boulanger', stat_arthur)
+let Potion = new Item('Potion','#', 'Une petite potion de soin', '1Go')
+let Bandages = new Item('Bandages', '#', 'Bandelettes de gaze. Rends 5pv', '5Go', )
+let Epee_divine = new Item('Epée divine', '#', `Selon la légende elle peut trancher le ciel`, '50000Go', )
+let Rapiere = new Item('Rapière', '#', `Légère et élégante, un arme digne d'un noble`, '500Go', )
+
+Arthur.empoche(Epee_divine)
+Alice.empoche(Rapiere)
+Alastar.empoche(Potion)
+Alastar.empoche(Bandages)
+
+
+const liste_personages = [Arthur, Alastar, Alice]
+const liste_items = [Potion, Bandages, Epee_divine, Rapiere]
+
+liste_items.sort((a, b) => (a.nom > b.nom) ? 1 : -1)
+liste_personages.sort((a, b) => (a.nom > b.nom) ? 1 : -1)
---
+++ script/script.js
@@ -0,0 +1,83 @@
+cl = console.log
+
+function dashboard(){
+	const 
+		header = { 
+			element : 'nav',
+			className : 'navbar navbar-dark bg-primary d-flex flex-row-reverse px-2',
+			enfants: [
+				{ element: 'button', innerText: 'Deconnexion', className:'btn btn-outline-light' },
+			]
+		}
+	
+		return header
+}
+
+function formControl(input){
+	return {
+		element: 'div', className: 'mb-3',
+		enfants: [
+			{ 
+				element: 'label', 
+				for: `input${input.name}`, 
+				className: 'form-label', 
+				innerText : `${input.text}`
+			},
+			{ 
+				element: 'input', 
+				type : `${input.type}`,
+				id: `input${input.name}`,
+				name: input.name,
+				className: 'form-control'
+			}
+		]
+	}
+}
+
+function formulaireConnexion(){
+	return {
+		element: 'form',
+		className: 'm-3',
+		enfants : [
+			{ element: "h2", innerText:"Connexion", className: "mb-3"},
+			formControl({name: "user", type: "text", text: "Nom d'utilisateur"}),
+			formControl({name: "password", type: "password", text: "Mot de passe"}),
+			{ element: "button", type: "submit", className: "btn btn-primary", innerText: "Connecter"}
+		]
+	}
+}
+
+function formulaireInscription(){
+	return {
+		element: 'form',
+		className: 'm-3',
+		enfants : [
+			{ element: "h2", innerText:"Inscription", className: "mb-3"},
+			formControl({name: "user", type: "text", text: "Nom d'utilisateur"}),
+			formControl({name: "password", type: "password", text: "Mot de passe"}),
+			formControl({name: "repeat_password", type: "password", text: "Repeter le Mot de passe"}),
+			{ element: "button", type: "submit", className: "btn btn-primary", innerText: "Inscription"}
+		]
+	}
+}
+
+function print(template, parent){
+	// cl(template)
+	let element = document.createElement(template.element)
+
+	for (let index in template ){
+		if (index == 'element')continue
+		else if (index == 'enfants'){ template[index].forEach( child => print( child, element) ) }
+		//here be the magic
+		else element[index] = template[index]
+	}
+	//element.innerText = 'tesst'
+
+	parent.appendChild(element)
+}
+
+const Root = document.querySelector('.rendu')
+print(dashboard(), Root)
+print({element: "div", className:"container main"}, Root)
+
+const Main = document.querySelector('.main')
