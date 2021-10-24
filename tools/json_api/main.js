9df3bf749ed1070e53a0c6f1fb2a47d45f1ec3b6
Ehacko <Darkdav.T@gmail.com>
2021-10-24 14:12:47 -0400

    api update

--- tools/json_api/main.js
+++ tools/json_api/main.js
@@ -4,8 +4,8 @@ const path = require('path');
 module.exports = ([table, id], data, action) => {
   const
     file_path = path.join(__dirname, `../../store/${table}.json`),
-    records = require(file_path) || [],
-    res = require(`./${action}`)(records, data, id);
+    records = fs.existsSync(file_path) ? require(file_path) : [],
+    res = (records && require(`./${action}`)(records, data, id) )|| 'NOT FOUND';
 
   console.log('res =>', res)
   fs.writeFileSync(file_path, JSON.stringify(records))
