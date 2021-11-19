910eab23a38fc32ecc3af7bd145b8f3c098f1c9c
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2021-11-19 19:59:35 -0400

    factorisation des routes

--- routes/index.js
+++ routes/index.js
@@ -2,40 +2,79 @@ const express = require('express');
 const router = express.Router();
 const api = require('../tools/json_api/main')
 
-function new_route(accumulator, item) {
-  accumulator[item.method](item.path, (req, res, next)=>{
+function new_route(item) {
+  this[item.method](item.path, (req, res, next)=>{
     res.render('index', {
-      title: 'Express',
+      title: item.name,
       params: { get: req.query, post: req.body },
+      ... (item.data||{})
     });
   })
 }
 
-/* 
+
 [
   {
     path: '/',
     method: 'get',
+    name: 'Express',
     page: 'index',
-    data: {foo: 'bars'}
-  }
-].reduce(new_route, router)
- */
+    data: {page: 'signin'}
+  },
+  {
+    path: '/home',
+    method: 'get',
+    name: 'Express',
+    page: 'index',
+    data: {home: true}
+  },
+  {
+    path: '/inscription',
+    method: 'get',
+    name: 'Express',
+    page: 'index',
+    data: {page: 'signup'}
+  },
+  {
+    path: '/logged',
+    method: 'get',
+    name: 'Express',
+    page: 'index',
+    data: {logged: true}
+  },
+].forEach( new_route.bind(router) )
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    page: 'signin'
-  });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', {
+//     title: 'Express',
+//     page: 'signin'
+//   });
+// });
 
-router.get('/home', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    home: true
-  });
-});
+// router.get('/home', function(req, res, next) {
+//   res.render('index', {
+//     title: 'Express',
+//     home: true
+//   });
+// });
+
+
+
+// router.get('/inscription', function(req, res, next) {
+//   res.render('index', {
+//     title: 'Express',
+//     page: 'signup'
+//   });
+// });
+
+
+// /* GET home page. */
+// router.all('/logged', function(req, res, next) {
+//   res.render('index', { title: 'Express', logged: true });
+// });
+
 router.post('/', function(req, res, next) {
   res.send(
     (api({data: req.body, action: 'read'})._id).toString()
@@ -61,19 +100,6 @@ router.delete('/store/*', function(req, res, next) {
   res.send(
     api(req.params[0].split('/'), req.body, 'delete')
   )
-});
-
-router.get('/inscription', function(req, res, next) {
-  res.render('index', {
-    title: 'Express',
-    page: 'signup'
-  });
-});
-
-
-/* GET home page. */
-router.all('/logged', function(req, res, next) {
-  res.render('index', { title: 'Express', logged: true });
 });
 
 module.exports = router;
