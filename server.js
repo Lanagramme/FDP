1a4cd615c6cdeea9c88300eb5517cfc579b94511
Lanagramme <lborromee16@gmail.com>
2021-11-19 21:20:15 -0400

    definition du server light

---
+++ server.js
@@ -0,0 +1,19 @@
+const 
+	express = require('express')
+	app = express()
+	path = require('path')
+
+app.get('/', (req,res)=>{
+	console.log('Connection ::')
+	res.sendFile(path.join(__dirname,'/client/index.html'))
+})
+
+app.get('/css/styles.css', (req,res) => {
+	res.sendFile(path.join(__dirname, '/client/css/styles.css'))
+})
+
+
+app.get('/js/*', (req,res)=>{
+	res.sendFile(path.join(__dirname, '/client/js/'+req.params[0] ))
+})
+app.listen('3001')
