cc2f39e70eb9ccc3e12a050ce07003369f47c2cd
Lanagramme <lborromee16@gmail.com>
2021-11-19 22:35:10 -0400

    redondances

--- public/javascripts/safenav.js
+++
@@ -1,8 +0,0 @@
-// Renvoie automatique ves la page d'accueil des utilisteurs non connectés
-
-const token = localStorage.getItem('token');
-
-if (!token) {
-  print({ element: "a", innerText : '', href : "/", id:"out" }, Main);
-  $('#out').click()
-}
--- readme.md
+++ readme.md
@@ -17,6 +17,7 @@
   [ ] tableau liste des parties
 
 ## Dashboard joueur
+
 ## Dashboard mj
 
 
