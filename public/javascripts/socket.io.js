e719673c85f429792fe532b828cdd0f108332b38
ehacko <darkdav.T@gmail.com>
2021-11-21 15:07:53 -0400

    add socket io room functionnalities

--- public/javascripts/socket.io.js
+++ public/javascripts/socket.io.js
@@ -1,3 +1,4 @@
+const room_function = {}
 function mysocket(socket, token="") {
   socket.on('connect', () => {
     socket
@@ -9,15 +10,21 @@ function mysocket(socket, token="") {
         console.log(`unauthorized: ${JSON.stringify(msg.data)}`);
         throw new Error(msg.data.type);
       })
+
+    room_function.join = (roomName) => socket.emit('joinRoom', roomName)
+    room_function.leave = (roomName) => socket.emit('leaveRoom', roomName)
+    room_function.open = (roomName) => socket.emit('openRoom', roomName)
+    room_function.close = (roomName) => socket.emit('closeRoom', roomName)
+    socket.on( 'user-msg', msg => alert(msg) )
   });
 }
 $.ajax({
   method: 'POST',
   url: '/users/login',
-  data: {testy: 'test'},
+  data: {email: 'test'},
 })
 .done(
-  res => mysocket(io.connect(), res.token)
+  res => (mysocket(io.connect(), res.token))
 )
 .fail( (jqXHR, textStatus, errorThrown) => {
   console.log(jqXHR, textStatus, errorThrown)
--- tools/socketio/main.js
+++ tools/socketio/main.js
@@ -14,7 +14,54 @@ io.on( "connection", /**socketioJwt.authorize({
 
   (socket.decoded_token && console.log(socket.decoded_token.email, 'has joined')) || console.log('Anonymous has joined') ;
   // console.log(socket.decoded_token);
+  socket.on('leaveRoom', function(roomName) {
+    socket.leave(roomName)
+    socket.on('leaveRoom', function(roomName) {
+      socket.on('leaveRoom', function(roomName) {
+        socket.leave(roomName)
+      });
+      console.log(socket.id)
+      socket.leave(roomName)
+    });
+    console.log(socket.id)
+  });
+  console.log(socket.id)
   //socket.on('event')
+
+
+  socket.on('getRoomsList', function() {
+    socket.emit( 'rooms', Object.keys(io.socket.adapter.rooms) );
+  });
+
+  io.sockets.adapter.on("create-room", (room) => {  console.log(`Room ${room} was created`);});
+  io.sockets.adapter.on("delete-room", (room) => {  console.log(`Room ${room} was deleted`);});
+  io.sockets.adapter.on("join-room", (room, id) => {  console.log(`Socket ${id} has joined room ${room}`);});
+  io.sockets.adapter.on("leave-room", (room, id) => {  console.log(`Socket ${id} has leaved room ${room}`);});
+  
+  socket.on('openRoom', function(roomName) {
+    socket.join(roomName)
+  });
+  
+  socket.on('joinRoom', function(roomName) {
+    console.log(socket.id)
+    if(Object.keys(io.sockets.adapter.rooms).includes(roomName)) socket.join(roomName); 
+    else socket.emit('user-msg', `La partie nommée "${roomName}" est introuvable`);
+  });
+
+  socket.on('closeRoom', function(roomName) {
+    io.in(roomName).socketsLeave(roomName);
+    socket.on('leaveRoom', function(roomName) {
+      socket.leave(roomName)
+    });
+    console.log(socket.id)
+    socket.emit('closeRoom', roomName)
+  });
+  
+  socket.on('leaveRoom', function(roomName) {
+    socket.leave(roomName)
+  });
+  console.log(socket.id)
+
 });
 // end of socket.io logic
 
